798d38fa647a842ac016d5383bd940b11a432387
anna mendoza <anna@curvecommunications.biz>
2016-07-08 16:06:51 -0700

    added the index.html and index.jsx

---
+++ index.jsx
@@ -0,0 +1,50 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Person = function() {  // kind of like a Class
+    var name = 'Derek Zoolander';   // attribute name
+    var imageUrl = 'http://uifaces.com/assets/static/images/zoolander.jpg';
+    var job = 'Male model';
+    return (
+        <div className="person">
+            <div className="person-name">{name}</div>  // kind of like #{var}
+            <img className="person-img" src={imageUrl} />
+            <div className="person-job">
+                {job}
+            </div>
+        </div>
+    );
+};
+
+var PersonList = function() {
+    return (
+        <div className="person-list">
+            <Person />
+            <Person />
+            <Person />
+            <Person />
+            <Person />
+        </div>
+    );
+};
+
+// statefull
+var PersonList = React.createClass({
+    render: function() {
+        var people = [];
+        for (var i=0; i<5; i++) {
+            people.push(<Person />);
+        }
+        return (
+            <div className="person-list">
+                {people}
+            </div>
+        );
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
+});
+
+
