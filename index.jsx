bbcc62c14fee78b4eb5d8bc7701809888b4bf41d
anna mendoza <anna@curvecommunications.biz>
2016-07-29 23:00:15 -0700

    I noticed that we not using the prop names, yet we as a group introduced prop as a perameter in the People function but i think that's a future refactor once we pass those props in from another component. This is where it gets a bit confusing. This almost demands a graphic. More to come here

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = function(props) {  // kind of like a Class
+var Person = function() {  // kind of like a Class
     var name = 'Derek Zoolander';   // attribute name
     var imageUrl = 'http://uifaces.com/assets/static/images/zoolander.jpg';
     var job = 'Male model'; //
@@ -20,7 +20,7 @@ var Person = function(props) {  // kind of like a Class
 var PersonList = React.createClass({
     render: function() {
         var people = [];
-        for (var i=0; i<5; i++) {
+        for (var i=0; i<16; i++) {
             people.push(<Person key={i} /> );
         }
         return (
